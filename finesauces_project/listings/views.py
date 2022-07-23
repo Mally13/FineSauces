9b93f222162c99f70d13ce322f6b6f2900bd538a
Mally13 <mallymutuku13@gmail.com>
2022-07-23 17:49:14 +0300

    new

--- finesauces_project/listings/views.py
+++ finesauces_project/listings/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+def product_list(request,category_slug=None):
+    categories=Category.objects.all()
+    requested_category=None
+    products=Product.objects.all()
+
+
+    if category_slug:
+        requested_category=get_object_or_404(Category, slug=category_slug)
+        products= Product.objects.filter(category=requested_category)
+
+    return render(
+        request,
+        'product/list.html',
+        {
+            'categories': categories,
+            'requested_category':requested_category,
+            'products':products
+        }
+    )
